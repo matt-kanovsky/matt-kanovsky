52bc7739bea3ce6f40fb9635024fc43ea36221db
Matt Kanovsky <matt.kanovsky@betterment.com>
2025-01-17 17:15:12 -0500

    enable ssr and update with files from remix template

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -1,7 +1,23 @@
 import { reactRouter } from '@react-router/dev/vite';
+import { cloudflareDevProxy } from '@react-router/dev/vite/cloudflare';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
-});
+export default defineConfig(({ isSsrBuild }) => ({
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+          input: './workers/app.ts',
+        }
+      : undefined,
+  },
+  plugins: [
+    cloudflareDevProxy({
+      getLoadContext({ context }) {
+        return { cloudflare: context.cloudflare };
+      },
+    }),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
+}));
