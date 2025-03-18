8a4bf5349d3be03d25b8d8a1145fe0888844c8ef
Matt Kanovsky <matt.kanovsky@gmail.com>
2025-03-18 16:34:35 -0400

    set up basic responsive tulip implementation

--- apps/website/app/routes/garden.tsx
+++ apps/website/app/routes/garden.tsx
@@ -10,7 +10,7 @@ export default function Garden() {
         background: '#ACD8E9',
         width: '100%',
         minHeight: '100dvh',
-        paddingY: '60px',
+        paddingY: '20px',
         alignContent: 'center',
         justifyContent: 'center',
         display: 'flex'
@@ -20,16 +20,19 @@ export default function Garden() {
         className={flex({
           flexDirection: 'column',
           justifySelf: 'center',
+          alignSelf: 'center',
           bg: '#008662',
           borderRadius: '20px',
           alignItems: 'center',
           justifyContent: 'center',
-          gap: '20px',
+          gap: '10px',
           width: '60%',
-          minWidth: '400px',
-          paddingTop: '40px',
-          paddingBottom: '60px',
-          paddingX: '80px'
+          minWidth: '280px',
+          paddingTop: { base: '20px', lg: '50px' },
+          paddingBottom: { base: '10px', lg: '30px' },
+          paddingX: '30px',
+          height: '80dvh',
+          minHeight: '560px'
         })}
       >
         <Link
@@ -37,14 +40,18 @@ export default function Garden() {
           className={css({
             color: 'white',
             fontFamily: 'LettraMono-Medium',
-            fontSize: '28px',
+            fontSize: {
+              base: '36px',
+              lg: '64px'
+            },
             textDecoration: 'none',
             _hover: { textDecoration: 'underline' }
           })}
         >
           enter
         </Link>
-        <TulipGarden rows={3} columns={3} />
+        <TulipGarden rows={3} columns={5} css={{ display: { base: 'none', lg: 'flex' } }} />
+        <TulipGarden rows={3} columns={3} css={{ display: { base: 'flex', lg: 'none' } }} />
       </div>
     </div>
   );
